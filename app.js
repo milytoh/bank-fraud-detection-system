aeb5b74619d752e16c88b967f42635879ebe832b
milytoh <milytohgold@gmail.com>
2026-06-04 04:57:20 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+require("dotenv").config();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// EJS setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("FraudShield System Running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
